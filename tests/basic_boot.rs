589e3e7c96577948c8597c58604c38ace7ee219b
Connor Brewster <cbrewster@hey.com>
2021-09-04 21:32:11 -0500

    Interrupts

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -4,31 +4,22 @@
 #![test_runner(brewos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use brewos::println;
 use core::panic::PanicInfo;
+
+use brewos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World!");
-
-    brewos::init();
-
-    #[cfg(test)]
     test_main();
-
-    println!("It did not crash!");
     brewos::hlt_loop();
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    brewos::hlt_loop();
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     brewos::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
